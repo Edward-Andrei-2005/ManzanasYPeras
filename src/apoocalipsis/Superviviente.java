4154907e20562c81f43148deed9aa8e30b421f86
AnassC7 <77987677+AnassC7@users.noreply.github.com>
2024-11-11 18:58:23 +0100

    +Atributos Superviviente.java

--- src/apoocalipsis/Superviviente.java
+++ src/apoocalipsis/Superviviente.java
@@ -4,6 +4,19 @@
  */
 package apoocalipsis;
 
+import java.util.ArrayList;
+
 public class Superviviente {
+    // Atributos
+    private String nombre;
+    private boolean estado;
+    private int accionesPorTurno;
+    private int heridas;
+    private Arma manoIzq;
+    private Arma manoDer;
+    private ArrayList<Equipo> inventario;
     
+    // Constructores
+    
+    // Métodos
 }
