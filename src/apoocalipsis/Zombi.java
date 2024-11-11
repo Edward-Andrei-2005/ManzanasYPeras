68d45c0a3db6d8deb67275135a9e3bd814611bee
Edward_Andrei <153552941+Edward-Andrei-2005@users.noreply.github.com>
2024-11-11 19:24:59 +0100

    Update Zombi.java
    Constructor de Zombi

--- src/apoocalipsis/Zombi.java
+++ src/apoocalipsis/Zombi.java
@@ -4,6 +4,75 @@
  */
 package apoocalipsis;
 
-public class Zombi {
+import static apoocalipsis.TipoZombi.ABOMINACION;
+import static apoocalipsis.TipoZombi.CAMINANTE;
+import static apoocalipsis.TipoZombi.CORREDOR;
+
+public abstract class Zombi {
+    //Atributos
+    private static int contador = 1;
+    private int identificador;
+    private int aguante, activaciones;
+    private String reaccionAnteAtaques;
     
+    //Constructores
+    public Zombi (TipoCaminante caminante, TipoZombi zombi) {
+        if((caminante != null) && (zombi != null)) {
+            identificador = contador++;     //Si se puede crear el zombie, le ponemos un identificador
+            switch(caminante) {             //Creamos el zombie dependiendo del tipo que es
+                
+                case NORMAL:
+                    switch(zombi) {
+                        case CAMINANTE:
+                            aguante = 1;
+                            activaciones = 1;
+                            break;
+                        case CORREDOR:
+                            aguante = 1;
+                            activaciones = 2;
+                            break;
+                        case ABOMINACION:
+                            aguante = 3;
+                            activaciones = 1;
+                            break;
+                    }
+                    break;
+                
+                case BERSERKER:
+                    switch(zombi) {
+                        case CAMINANTE:
+                            aguante = 1;
+                            activaciones = 1;
+                            break;
+                        case CORREDOR:
+                            aguante = 1;
+                            activaciones = 2;
+                            break;
+                        case ABOMINACION:
+                            aguante = 3;
+                            activaciones = 1;
+                            break;
+                    }
+                    break;
+                
+                case TOXICO:
+                    switch(zombi) {
+                        case CAMINANTE:
+                            aguante = 1;
+                            activaciones = 1;
+                            break;
+                        case CORREDOR:
+                            aguante = 1;
+                            activaciones = 2;
+                            break;
+                        case ABOMINACION:
+                            aguante = 3;
+                            activaciones = 1;
+                            break;
+                    }
+                    break;
+            }
+                    
+        }
+    }
 }
