ff0e6d95ac26bcd1dd71f41c9440607975f3a3b5
TeraByte1910 <153093911+TeraByte1910@users.noreply.github.com>
2024-11-11 20:18:21 +0100

    Update Provision.java
    Constantes básicas y constructor

--- src/apoocalipsis/Provision.java
+++ src/apoocalipsis/Provision.java
@@ -1,9 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package apoocalipsis;
 
-public class Provision {
+public class Provision extends Equipo {
+    private String nombre;
+    private int valorEnergetico;
+    private int caducidad;
     
+    private static final String[] NOMBRES = {"Espaguetis", "Caviar", "Mixto", "Empanada", "Manzana"};
+    private static final int KCAL_MIN = 50;
+    private static final int KCAL_MAX = 200;
+    private static final int CADUCIDAD_MIN = 2;
+    private static final int CADUCIDAD_MAX = 15;
+    
+    public Provision() {
+        nombre = NOMBRES[(int) (Math.random() * NOMBRES.length)];
+        valorEnergetico = (int) (Math.random() * (KCAL_MAX-KCAL_MIN)) + KCAL_MIN;
+        caducidad = (int) (Math.random() * (CADUCIDAD_MAX-CADUCIDAD_MIN)) + CADUCIDAD_MIN;
+    }
 }
