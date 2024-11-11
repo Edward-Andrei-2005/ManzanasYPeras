2c06dfa6ed396cca4c5efbc3512b2cf090a129e6
AnassC7 <77987677+AnassC7@users.noreply.github.com>
2024-11-11 19:31:34 +0100

    +Constructor Superviviente.java

--- src/apoocalipsis/Superviviente.java
+++ src/apoocalipsis/Superviviente.java
@@ -9,14 +9,43 @@ import java.util.ArrayList;
 public class Superviviente {
     // Atributos
     private String nombre;
-    private boolean estado;
-    private int accionesPorTurno;
-    private int heridas;
+    private boolean estado; // vivo = true, eliminado = false
+    private static final int accionesPorTurno = 3;
+    private int contZombisKO;
+    private int heridas; // Un Superviviente es eliminado al recibir 2 heridas o mordeduras.
     private Arma manoIzq;
     private Arma manoDer;
-    private ArrayList<Equipo> inventario;
+    
+    private static final int TAM = 5; // Tamaño máximo de Equipo [0..5]
+    private Equipo inventario[];
     
     // Constructores
+    public Superviviente(String n) {
+        nombre = n;
+        estado = true;
+        inventario = new Equipo[TAM];
+    }
     
     // Métodos
+    
+    /* Buscar equipo en la casilla actual. Se obtiene, de forma aleatoria, una instancia de
+    equipo. No se puede buscar dos veces en la misma casilla. */
+    public Equipo buscar() {
+        
+    }
+    
+    // // Usar un arma equipada para atacar zombis en una casilla específica.
+    public void atacarArma(Arma arma, Casilla casilla) {
+        
+    }
+    
+    // Cambiar un arma activa de entre las armas que lleva en su inventario.
+    public void elegirArma(boolean izq) {
+        
+    }
+    
+    // El Superviviente puede optar por no realizar ninguna acción.
+    public void noHacerNada() {
+        
+    }
 }
