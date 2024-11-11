8cd42fb6437fd2adcb01c2c97d6b1f12b8aae94c
AnassC7 <77987677+AnassC7@users.noreply.github.com>
2024-11-11 20:10:31 +0100

    +Constructor Casilla.java

--- src/apoocalipsis/Casilla.java
+++ src/apoocalipsis/Casilla.java
@@ -8,4 +8,12 @@ public class Casilla {
     // Atributos
     private int x;
     private int y;
+    
+    // Constructores
+    public Casilla(int x, int y) {
+        if (x > 0 && y > 0) { // Los valores de x e y solo pueden ser positivos
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
