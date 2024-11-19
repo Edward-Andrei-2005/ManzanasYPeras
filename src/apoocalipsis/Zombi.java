44093111d4adb61ddc5283189750381bf76063e6
Edward_Andrei <153552941+Edward-Andrei-2005@users.noreply.github.com>
2024-11-19 13:06:05 +0100

    Update Zombi.java
    Clases Zombi cambiadas a herencia

--- src/apoocalipsis/Zombi.java
+++ src/apoocalipsis/Zombi.java
@@ -1,9 +1,5 @@
 package apoocalipsis;
 
-import static apoocalipsis.TipoZombi.ABOMINACION;
-import static apoocalipsis.TipoZombi.CAMINANTE;
-import static apoocalipsis.TipoZombi.CORREDOR;
-
 public abstract class Zombi {
     //Atributos
     private static int contador = 1;
@@ -12,64 +8,22 @@ public abstract class Zombi {
     private String reaccionAnteAtaques;
     
     //Constructores
-    public Zombi (TipoCaminante caminante, TipoZombi zombi) {
-        if((caminante != null) && (zombi != null)) {
-            identificador = contador++;     //Si se puede crear el zombie, le ponemos un identificador
-            switch(caminante) {             //Creamos el zombie dependiendo del tipo que es
-                
-                case NORMAL:
-                    switch(zombi) {
-                        case CAMINANTE:
-                            aguante = 1;
-                            activaciones = 1;
-                            break;
-                        case CORREDOR:
-                            aguante = 1;
-                            activaciones = 2;
-                            break;
-                        case ABOMINACION:
-                            aguante = 3;
-                            activaciones = 1;
-                            break;
-                    }
-                    break;
-                
-                case BERSERKER:
-                    switch(zombi) {
-                        case CAMINANTE:
-                            aguante = 1;
-                            activaciones = 1;
-                            break;
-                        case CORREDOR:
-                            aguante = 1;
-                            activaciones = 2;
-                            break;
-                        case ABOMINACION:
-                            aguante = 3;
-                            activaciones = 1;
-                            break;
-                    }
-                    break;
-                
-                case TOXICO:
-                    switch(zombi) {
-                        case CAMINANTE:
-                            aguante = 1;
-                            activaciones = 1;
-                            break;
-                        case CORREDOR:
-                            aguante = 1;
-                            activaciones = 2;
-                            break;
-                        case ABOMINACION:
-                            aguante = 3;
-                            activaciones = 1;
-                            break;
-                    }
-                    break;
-            }
+    public Zombi (int aguante, int activaciones) {
+        if((aguante>=1) && (aguante<=3) && (activaciones>=1) && (activaciones<=2)) {
+            identificador = contador++;
+            this.aguante = aguante;
+            this.activaciones = activaciones;
         } else {
-            identificador = -1; //Si algún parámetro es incorrecto ponemos el id en -1
+            identificador = -1;
         }
+    }
+    
+    //Métodos
+    public void setAguante(int aguante) {
+        this.aguante = aguante;
+    }
+
+    public void setActivaciones(int activaciones) {
+        this.activaciones = activaciones;
     }
 }
