6838b625cdf740af652b7ca88a7117c6aea4d929
Edward_Andrei <153552941+Edward-Andrei-2005@users.noreply.github.com>
2024-11-19 17:22:48 +0100

    Update Corredor.java

--- src/apoocalipsis/Corredor.java
+++ src/apoocalipsis/Corredor.java
@@ -1,8 +1,11 @@
 package apoocalipsis;
 
 public abstract class Corredor extends Zombi {
+    //Atributos
+    private static final int AGUANTE = 1;
+    private static final int ACTIVACIONES = 2;
     //Constructores
-    public Corredor(int aguante, int activaciones) {
-        super(aguante, activaciones);
+    public Corredor() {
+        super(AGUANTE, ACTIVACIONES);
     }
 }
