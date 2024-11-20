e954fa7f4ec69a4697cd4af978a3f496e14a3425
TeraByte1910 <153093911+TeraByte1910@users.noreply.github.com>
2024-11-20 17:15:37 +0100

    Update Casilla.java

--- src/apoocalipsis/Casilla.java
+++ src/apoocalipsis/Casilla.java
@@ -1,15 +1,20 @@
 package apoocalipsis;
+import java.util.ArrayList;
 
 public class Casilla {
     // Atributos
     private int x;
     private int y;
+    private ArrayList <EntidadActivable> listaEntidades;
     
     // Constructores
     public Casilla(int x, int y) {
         if (x > 0 && y > 0) { // Los valores de x e y solo pueden ser positivos
             this.x = x;
             this.y = y;
+            listaEntidades = new ArrayList <> ();
         }
     }
+    
+    
 }
