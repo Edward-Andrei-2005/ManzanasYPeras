01eadad2d3936850d12262dffaadfc0d798724ba
TeraByte1910 <153093911+TeraByte1910@users.noreply.github.com>
2024-11-20 19:19:40 +0100

    Update Juego.java

--- src/apoocalipsis/Juego.java
+++ src/apoocalipsis/Juego.java
@@ -1,46 +1,66 @@
 package apoocalipsis;
 
 public class Juego {
-    // Atributos
     private boolean turno;
     private Casilla[][] dimension;
     
     private static final int TAM = 10;
     
-    // Constructores
     public Juego() {
         dimension = new Casilla [TAM][TAM];
     }
     
-    // Métodos
-    private Arma elegirArma(Superviviente superviviente, boolean izq) {
+    public boolean generarAtaque(Superviviente s, boolean izq, Casilla objetivo) {
+        Arma a = elegirArma(s, izq);
+        // Si es null abortar
+        
+        Casilla origen = buscarCasillaOrigen(s);
+        
+        return false;
+    }
+    
+    private Arma elegirArma(Superviviente s, boolean izq) {
         for (int i = 0; i < TAM; i++) {
             for (int j = 0; j < TAM; j++) {
-                Iterator<EntidadActivable> it = dimension[i][j]..iterator();
+                Arma a = dimension[i][j].buscarArma(s, izq);
+                if (a != null) return a;
             }
         }
-        
         return null;
     }
     
-    private boolean seleccionarObjetivo() {
-        return false;
+    private Casilla buscarCasillaOrigen(Superviviente s) {
+        for (int i=0; i<TAM; i++) {
+            for (int j=0; j<TAM; j++) {
+                if (dimension[i][j].estaSuperviviente(s)) return dimension[i][j];
+            }
+        }
     }
     
-    private boolean lanzarDados() {
-        return false;
+    private boolean seleccionarObjetivo(Casilla origen, Arma a, Casilla destino) {
+        return a.puedeAtacar(origen.distancia(destino));
     }
     
-    private boolean evaluarExito() {
-        return false;
+    private int[] lanzarDados(Arma a) {
+        int[] v = new int [a.getNumeroDeDados()];
+        for (int i=0; i<v.length; i++) {
+            v[i] = a.tirarDado();
+        }
+        return v;
+    }
+    
+    private int evaluarExito(int[] v, Arma a) {
+        int contador = 0;
+        for (int i=0; i<v.length; i++) {
+            if (a.getValorDeExito() <= v[i]) contador++;
+        }
+        return contador;
     }
     
     private boolean resolverAtaque() {
-        return false;
+            
     }
     
-    public boolean generarAtaque() {
-        return false;
-    }
+    
     
 }
