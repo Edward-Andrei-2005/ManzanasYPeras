a746f6340aaed2426110d1b8a0937017c1298a58
TeraByte1910 <153093911+TeraByte1910@users.noreply.github.com>
2024-11-20 17:17:02 +0100

    Create Juego.java

---
+++ src/apoocalipsis/Juego.java
@@ -0,0 +1,37 @@
+package apoocalipsis;
+
+public class Juego {
+    private boolean turno;
+    private Casilla[][] dimension;
+    
+    private static final int TAM = 10;
+    
+    public Juego() {
+        dimension = new Casilla [TAM][TAM];
+    }
+    
+    public boolean seleccionarObjetivo() {
+        return false;
+    }
+    
+    public boolean elegirArma() {
+        
+    }
+    
+    public boolean lanzarDados() {
+        
+    }
+    
+    public boolean evaluarExito() {
+        
+    }
+    
+    public boolean resolverAtaque() {
+        
+    }
+    
+    public boolean generarAtaque() {
+        
+    }
+    
+}
