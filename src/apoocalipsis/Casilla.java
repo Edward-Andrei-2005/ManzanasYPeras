33a05063f80ee7ce575b99c2c52f00a7b852d646
TeraByte1910 <153093911+TeraByte1910@users.noreply.github.com>
2024-11-20 19:19:10 +0100

    Update Casilla.java

--- src/apoocalipsis/Casilla.java
+++ src/apoocalipsis/Casilla.java
@@ -15,6 +15,38 @@ public class Casilla {
             listaEntidades = new ArrayList <> ();
         }
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
     
     
+    
+    // Devuelve el arma del superviviente. Devuelve null si da error
+    // Hacer try catch
+    public Arma buscarArma(Superviviente s, boolean izq) {
+        if (estaSuperviviente(s)) {
+            int i = listaEntidades.indexOf(s);
+            Superviviente sup = (Superviviente) listaEntidades.get(i);
+            if (izq) {
+                return sup.getManoIzq();
+            } else {
+                return sup.getManoDer();
+            }
+        } else {
+            return null;
+        }
+    }
+    
+    public boolean estaSuperviviente(Superviviente s) {
+        return listaEntidades.contains(s);
+    }
+    
+    public int distancia(Casilla c) {
+        return (int) Math.sqrt(Math.pow(x-c.getX(), 2) + Math.pow(y-c.getY(), 2));
+    }
 }
