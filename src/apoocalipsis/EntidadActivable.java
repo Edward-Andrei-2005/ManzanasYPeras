9eb445caeb02b7f85d250520b2d21f2eab7d28cb
TeraByte1910 <153093911+TeraByte1910@users.noreply.github.com>
2024-11-28 23:42:50 +0100

    Update EntidadActivable.java

--- src/apoocalipsis/EntidadActivable.java
+++ src/apoocalipsis/EntidadActivable.java
@@ -1,7 +1,4 @@
 package apoocalipsis;
 
 public abstract class EntidadActivable {
-    public abstract boolean activarse();
-    public abstract boolean moverse();
-    public abstract boolean atacar();
 }
