475d8b65d91c70e054c5f3650f30d27f8c3a28b8
AnassC7 <77987677+AnassC7@users.noreply.github.com>
2024-12-01 00:08:32 +0100

    Update Zombi.java

--- src/apoocalipsis/Zombi.java
+++ src/apoocalipsis/Zombi.java
@@ -6,10 +6,15 @@ public abstract class Zombi extends EntidadActivable {
     private int identificador;
     private int aguante, activaciones;
     private String reaccionAnteAtaques;
+    
+    private static final int MIN_AGUANTE = 1;
+    private static final int MAX_AGUANTE = 3;
+    private static final int MIN_ACTIVACIONES = 1;
+    private static final int MAX_ACTIVACIONES = 2;
 
     //Constructores
     public Zombi (int aguante, int activaciones) {
-        if((aguante>=1) && (aguante<=3) && (activaciones>=1) && (activaciones<=2)) {
+        if((aguante>=MIN_AGUANTE) && (aguante<=MAX_AGUANTE) && (activaciones>=MIN_ACTIVACIONES) && (activaciones<=MAX_ACTIVACIONES)) {
             identificador = contador++;
             this.aguante = aguante;
             this.activaciones = activaciones;
@@ -18,24 +23,22 @@ public abstract class Zombi extends EntidadActivable {
         }
     }
     
-
     // Métodos
-    public boolean activar() {
-        return true;
-    }
-    
     public boolean reaccionar() {
         return true;
     }
+
+    public int getActivaciones() {
+        return activaciones;
+    }
     
-    public boolean activarse() {
-        return true;
+    public void quitarActivacionesZombi() {
+        --this.activaciones;
     }
     
     public boolean esMatable(Arma a) {
         // Verifica si el zombi es de tipo CaminanteBerserker, CorredorBerserker o AbominacionBerserker
-        if((this instanceof CaminanteBerserker) || (this instanceof CorredorBerserker)
-                || (this instanceof AbominacionBerserker)) {
+        if((this instanceof CaminanteBerserker) || (this instanceof CorredorBerserker) || (this instanceof AbominacionBerserker)) {
             /*Si es un zombi Berserker, debe cumplir dos condiciones:
             1. El arma debe tener suficiente potencia para eliminar al zombi (potencia >= aguante)
             2. El arma debe ser cuerpo a cuerpo (alcance == 0), ya que los zombis Berserker son inmunes a ataques a distancia.*/
@@ -46,4 +49,3 @@ public abstract class Zombi extends EntidadActivable {
         }
     }
 }
-
