20e3699dc1eee83b55738e0dddb55d10f6480a0a
TeraByte1910 <153093911+TeraByte1910@users.noreply.github.com>
2024-12-05 22:02:01 +0100

    Update Corredor.java

--- src/apoocalipsis/Corredor.java
+++ src/apoocalipsis/Corredor.java
@@ -2,10 +2,14 @@ package apoocalipsis;
 
 public class Corredor extends Zombi {
     //Atributos
-    private static final int AGUANTE = 1;
-    private static final int ACTIVACIONES = 2;
+    protected static final int AGUANTE = 1;
+    protected static final int ACTIVACIONES = 2;
     //Constructores
     public Corredor() {
         super(AGUANTE, ACTIVACIONES);
+    }
+    
+    public boolean esMatable(Arma a) {
+        return (a.getPotencia() >= AGUANTE);
     }
 }
