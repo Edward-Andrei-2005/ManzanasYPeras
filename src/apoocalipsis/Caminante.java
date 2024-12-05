81ebeb3169a560037e107a40cf3eb8e6f091ff76
TeraByte1910 <153093911+TeraByte1910@users.noreply.github.com>
2024-12-05 22:03:14 +0100

    Update Caminante.java

--- src/apoocalipsis/Caminante.java
+++ src/apoocalipsis/Caminante.java
@@ -2,10 +2,14 @@ package apoocalipsis;
 
 public class Caminante extends Zombi {
     //Atributos
-    private static final int AGUANTE = 1;
-    private static final int ACTIVACIONES = 1;
+    protected static final int AGUANTE = 1;
+    protected static final int ACTIVACIONES = 1;
     //Constructores
     public Caminante() {
         super(AGUANTE, ACTIVACIONES);
+    }
+    
+    public boolean esMatable(Arma a) {
+        return a.getPotencia() >= AGUANTE;
     }
 }
