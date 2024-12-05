75657b53b8ec1bebb511367babb9d47e8c8aa7d0
TeraByte1910 <153093911+TeraByte1910@users.noreply.github.com>
2024-12-05 22:04:32 +0100

    Update Abominacion.java

--- src/apoocalipsis/Abominacion.java
+++ src/apoocalipsis/Abominacion.java
@@ -2,11 +2,15 @@ package apoocalipsis;
 
 public class Abominacion extends Zombi {
     //Atributos
-    private static final int AGUANTE = 3;
-    private static final int ACTIVACIONES = 1;
+    protected static final int AGUANTE = 3;
+    protected static final int ACTIVACIONES = 1;
     
     //Constructores
     public Abominacion() {
         super(AGUANTE, ACTIVACIONES);
+    }
+    
+    public boolean esMatable(Arma a) {
+        return a.getPotencia() >= AGUANTE;
     }
 }
