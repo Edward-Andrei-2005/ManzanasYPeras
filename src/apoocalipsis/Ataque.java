20e9e26f01b0147759374cf91bcdc61cb6b8ba7f
AnassC7 <77987677+AnassC7@users.noreply.github.com>
2024-12-14 02:05:19 +0100

    Update Ataque.java

--- src/apoocalipsis/Ataque.java
+++ src/apoocalipsis/Ataque.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package apoocalipsis;
 
 import java.io.Serializable;
 
-/**
- *
- * @author anass
- */
 public class Ataque implements Serializable {
    
     private Superviviente superviviente;
