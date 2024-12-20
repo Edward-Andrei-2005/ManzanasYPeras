7d4d3315bd060751586da8948c5e04e5be82be00
AnassC7 <77987677+AnassC7@users.noreply.github.com>
2024-12-20 21:51:23 +0100

    Update Equipo.java

--- src/apoocalipsis/Equipo.java
+++ src/apoocalipsis/Equipo.java
@@ -1,7 +1,10 @@
 package apoocalipsis;
 
 import java.io.Serializable;
-
+/**
+ *
+ * @author Anass Chikou El Mahraoui, Edward Andrei, Manuel Alos de la Vega
+ */
 public abstract class Equipo implements Serializable{
     public abstract String getInfo();
 }
