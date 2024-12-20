464e0813213608ed9243ca01cb3619a53362cfbe
AnassC7 <77987677+AnassC7@users.noreply.github.com>
2024-12-20 21:52:47 +0100

    Update J_ConfirmacionSupervivientes.java

--- src/apoocalipsis/J_ConfirmacionSupervivientes.java
+++ src/apoocalipsis/J_ConfirmacionSupervivientes.java
@@ -1,4 +1,8 @@
 package apoocalipsis;
+/**
+ *
+ * @author Anass Chikou El Mahraoui, Edward Andrei, Manuel Alos de la Vega
+ */
 public class J_ConfirmacionSupervivientes extends javax.swing.JPanel {
     private static final int TAM = 4;
 
