9573f419b570ae95287b6418aaae92da196a2df3
AnassC7 <77987677+AnassC7@users.noreply.github.com>
2024-12-20 21:51:16 +0100

    Update EntidadActivable.java

--- src/apoocalipsis/EntidadActivable.java
+++ src/apoocalipsis/EntidadActivable.java
@@ -1,7 +1,10 @@
 package apoocalipsis;
 
 import java.io.Serializable;
-
+/**
+ *
+ * @author Anass Chikou El Mahraoui, Edward Andrei, Manuel Alos de la Vega
+ */
 public abstract class EntidadActivable implements Serializable {
     public abstract String getInfo() ;
 }
